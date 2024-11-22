9245035eb08046f789f3caea78401889bc3902a1
seonyeonghun2 <kjca1117@naver.com>
2024-11-22 15:15:13 +0900

    서버측 코드 추가 및 변경

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,14 @@
 import express from "express"
+import path from "path"
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const app = express()
-const port = 4321
-
+const port = 3000
+app.use(express.static('public'))
 app.get('/', (req, res) => {
-  res.send('<h1>Hello World!</h1>')
+  // res.send('<h1>Hello World!</h1>')
+  res.sendFile(path.join(__dirname, "notice.html")); // 서버 점검을 알리는 웹문서를 클라이언트에 전송하는 명령
 })
 
 app.listen(port, () => {
