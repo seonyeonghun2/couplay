ac99fe9ce0fc969394fdbffb7b287f87587faddd
seonyeonghun2 <kjca1117@naver.com>
2024-11-27 17:07:11 +0900

    로그인 및 쿠키를 이용한 로그인정보 유지 구현

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import path from "path";
+import cookieParser from "cookie-parser"
 import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -45,6 +46,7 @@ const userSchema = new mongoose.Schema({
   },
   id: {
     type: String,
+    unique: true,
     minlength: 5,
     required: true,
     lowercase: true,
@@ -75,6 +77,7 @@ const userSchema = new mongoose.Schema({
 
 const User = mongoose.model("User", userSchema); // 실제 DB에서는 user 저장됨
 
+app.use(cookieParser());
 app.use(express.static("public"));
 app.use(express.json()); // 요청Body를 express가 처리(=이해)
 app.use(express.urlencoded({ extended: true }));
@@ -108,8 +111,16 @@ app.post("/signup", (req, res) => {
       if (!user) {
         throw new Error("사용자 생성 실패!");
       }
-      // 클라이언트에게 응답
-      res.status(200).json(user);
+      // 클라이언트에게 응답 : 쿠키/토큰 (클라이언트로 전송)
+      /*
+      res.status(200).json({
+        status: 'ok',
+        message: "회원 가입 성공",        
+      });
+      */
+     // 회원가입 성공하면, 현재 회원의 ID등의 정보를 Cookie로 저장해서 Client에 전송!
+     // Client가 Server에 요청(Request)할 때 쿠키 정보를 HTTP Header에 담아서 전송해줌
+     res.send('<script>alert("회원가입 성공, 홈으로 이동합니다.");location.href="/";</script>')
     });
   } catch (err) {
     console.log(err);
@@ -117,9 +128,59 @@ app.post("/signup", (req, res) => {
   }
 });
 
-app.post("/signin", (req, res) => {
-  res.send("/signin 페이지를 보고 계십니다.");
+app.post("/signin", async (req, res) => {
+  try {
+    const {user_id, user_pwd} = req.body  
+    const foundUser = await User.find({ id: user_id });
+    if (foundUser.length == 0) {
+      res.status(401).json({
+        status: 'fail',
+        message: "사용자가 존재하지 않습니다."
+      })
+    }
+    const match = await bcrypt.compare(user_pwd, foundUser[0].pwd)
+    if (match) {
+      // 로그인 성공 쿠키 : 사용자ID, 이름등의 로그인 정보를 기록
+      const loginInfo = JSON.stringify({
+        login_id: foundUser[0].id,
+        user_name: foundUser[0].name,
+        user_email: foundUser[0].email,
+        user_createdAt: foundUser[0].createdAt
+      })
+      res.cookie('login_user', foundUser[0].id, {
+        httpOnly: true,
+        maxAge: 60000        
+      })
+      res.status(200).json({
+        status: 'success',
+        message: '로그인 성공'
+      })
+    } else {
+      res.status(200).json({
+        status: 'fail',
+        message: '로그인 실패'
+      })
+    }
+    // res.send("/signin 페이지를 보고 계십니다.");
+  } catch (err) {
+    console.log(err);
+  }
 });
+
+app.get("/bucket", (req, res) => {
+  try {
+    if (req.cookies.login_user) {      
+      res.send("쿠키정보가 존재합니다")
+    } else {
+      res.status(401).json({
+        status: "fail",
+        message: "로그인이 필요합니다"
+      }) 
+    }
+  } catch (err) {
+    console.log(err)
+  }
+})
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
